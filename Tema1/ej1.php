24842022f8a323de5a765c144b29bf1f521435ce
victorfdez <vicfernandezespana@gmail.com>
2023-09-20 10:15:19 +0200

    Ejercicio 1 Hola Mundo con comentarios

--- Tema1/ej1.php
+++ Tema1/ej1.php
@@ -1,2 +1,23 @@
-<?php
-echo "Hola Mundo en PHP, hecho por Víctor Fernández España";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Victor Fernandez EJ1</title>
+</head>
+<body>
+    <center>
+    <h1>
+    <?php // Este es un comentario de apertura de código PhP
+        echo "Hola Mundo en PHP, hecho por Víctor Fernández España"; // Este es un comentario para mostrar ese mensaje con echo en Php
+        // Utilizamos el cierre porque tenemos codigo HMTL
+    ?> 
+    </h1>
+    <h3><a href="https://github.com/victorfernandezesp/DEWS/blob/main/Tema1/ej1.php">
+        <?php
+        echo "Link de Github al Ejercicio"; 
+        ?>
+    </a></h3>
+    </center>
+</body>
+</html>
